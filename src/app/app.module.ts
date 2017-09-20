097e64b6b5f9a4e87e7216c49ef241cb56533283
Duy Hung Du <30711265+duyhungdu@users.noreply.github.com>
2017-09-19 20:49:05 -0600

    change HashLocationStrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 //chrome.exe --remote-debugging-port=9222
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 
 import { APP_CONFIG, AppConfig } from './configs';
@@ -50,7 +51,7 @@ const APP_DIRECTIVES = [
 
 
 // Import routing module
-import { AppRoutingModule } from './app.routing';
+import { AppRoutingModule } from './app-routing.module';
 
 // Import 3rd party components
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
@@ -76,7 +77,10 @@ import { HttpModule } from '@angular/http';
     TabsModule.forRoot(),
     ChartsModule
   ],
-  providers: [AuthGuard, AuthenticationService, { provide: APP_CONFIG, useValue: AppConfig }],
+  providers: [AuthGuard,
+    AuthenticationService,
+    { provide: APP_CONFIG, useValue: AppConfig },
+    { provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
